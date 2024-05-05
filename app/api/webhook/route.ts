36351e114e49c96fbfc9cff031de81081114a2d6
Aditya Kakarla <adityakakarla@Adityas-Air.attlocal.net>
2024-05-04 17:44:41 -0700

    updated stripe requests

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -1,28 +1,26 @@
 export const dynamic = 'force-dynamic'
 
-import { NextApiRequest } from "next";
-import { NextRequest } from "next/server";
 import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
 
 const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET as string;
 
-export async function POST(request: NextApiRequest) {
+export async function POST(request: Request) {
   if (request.method !== "POST") {
     return new Response("Only allow POST methods", {
       status: 405,
     });
   }
 
-  const sig = request.headers["stripe-signature"] as string;
+  const sig = request.headers.get("stripe-signature") as string;
   if (!request.body) {
     return new Response("Invalid request, body is missing", { status: 400 });
   }
   let event;
 
   try {
-    const body = await buffer(request)
+    const body = await request.text()
     event = stripe.webhooks.constructEvent(body, sig, endpointSecret);
   } catch (err: any) {
     return new Response(`Webhook Error: ${err.message}`, { status: 400 });
@@ -34,19 +32,4 @@ export async function POST(request: NextApiRequest) {
   }
 
   return new Response("Event is accepted, but no need to update anything");
-}
-
-const buffer = (req: NextApiRequest) => {
-  return new Promise<Buffer>((resolve, reject) => {
-    const chunks: Buffer[] = [];
-    req.on('data', (chunk: Buffer) => {
-      chunks.push(chunk);
-    })
-
-    req.on('end', () => {
-      resolve(Buffer.concat(chunks))
-    })
-
-    req.on('error', reject)
-  })
 }
